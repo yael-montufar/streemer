fb1d9d1d344187ca769dd6acc9ecca3302454666
Yael Montufar <yael.montufar@gmail.com>
2020-03-04 14:02:33 -0600

    Refactored code for perpormer reviews

--- app/controllers/performers_controller.rb
+++ app/controllers/performers_controller.rb
@@ -2,10 +2,7 @@ class PerformersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def show
+    @performer = User.find(params[:id])
     @review = Review.new
-    @user = User.find(params[:id])
-    @performer = PerformerProfile.find(params[:id])
-    @reviews = @performer.reviews.last(5)
-    # @reviews = @performer.reviews.first(5) Not working!!!
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   def create
     @performer = User.find(params[:performer_id])
     @review = Review.new(review_params)
-    @review.performer_profile = PerformerProfile.find(params[:performer_id])
+    @review.performer_profile = PerformerProfile.find_by(user_id: @performer)
     if @review.save
       redirect_to performer_path(@performer)
     else
