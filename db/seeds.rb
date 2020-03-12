336229e29c8e13b565bf00dae8e812c80e163937
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2020-03-12 17:00:38 -0600

    any changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,41 +12,75 @@ performer_1 = PerformerProfile.create!(
   stage_name: "Some Herd Trouble",
   bio: "From the moment Some Herd Trouble starts playing, people are drawn to the warm sounds they create.  As the lead Rhys sits amongst his instruments you feel the rhythmic pulse of the stomp box, and the haunting bluesy sounds of the harmonica.  His guitar work playing underneath while his raw, powerful and emotive vocals soar above."
 )
+file = URI.open("https://res.cloudinary.com/yaelmontufar/image/upload/v1583980559/User%20profiles/29386816_10211679299598161_8683443718025379840_n_c1t5ol.jpg")
+   performer_1.photo.attach(io: file, filename: 'chello.jpg', content_type: 'image/jpg')
+   performer_1.save!
+
 performer_2 = PerformerProfile.create!(
   user: User.create!(email: "performer2@email.com", password: "123456"),
   stage_name: "The Space Cowboy",
   bio: "Since the Space Cowboy was just eight years old he has been perfecting the art of street theatre. His unique and incredibly skilled performance has been seen all around the world."
 )
+file = URI.open("https://res.cloudinary.com/yaelmontufar/image/upload/v1583980524/User%20profiles/89608535_2514402218823793_4208617828917444608_o_o9k7e6.jpg")
+    performer_2.photo.attach(io: file, filename: 'chello.jpg', content_type: 'image/jpg')
+    performer_2.save!
+
+
 performer_3 = PerformerProfile.create!(
   user: User.create!(email: "performer3@email.com", password: "123456"),
   stage_name: "FloorLegendz",
   bio: "Floor LegendZ is a break dance crew taking over the streets of Mexico. Fun, vibrant and full of talent."
 )
+file = URI.open("https://res.cloudinary.com/yaelmontufar/image/upload/v1584030580/User%20profiles/download_tq7wwz.jpg")
+    performer_3.photo.attach(io: file, filename: 'chello.jpg', content_type: 'image/jpg')
+    performer_3.save!
+
 performer_4 = PerformerProfile.create!(
   user: User.create!(email: "performer4@email.com", password: "123456"),
   stage_name: "Angel",
   bio: "Charlotte is a singer / songwriter and extrememly poite and cheery street performer around Mexico City.  Charlotte writes observational dittys that will haunt your soul and takes her songs to the streets."
 )
+file = URI.open("https://res.cloudinary.com/yaelmontufar/image/upload/v1583980751/User%20profiles/T02NE0241-U9B7VAVS8-d179a8be630c-512_agahxd.jpg")
+    performer_4.photo.attach(io: file, filename: 'chello.jpg', content_type: 'image/jpg')
+    performer_4.save!
+
+
 performer_5 = PerformerProfile.create!(
   user: User.create!(email: "performer5@email.com", password: "123456"),
   stage_name: "Tassel",
   bio: "Tassel is a singer-songwriter who moved to Mexico City in 2005.  He started playing guitar and writing songs in his teens and has performed ever since, playing regular gigs he started busking ever since. His songwriting is influenced by the folk singer tradition of Central America."
 )
+file = URI.open("https://res.cloudinary.com/yaelmontufar/image/upload/v1583980615/User%20profiles/60338632_10217742105094665_8739176910170357760_n_a1xaua.jpg")
+    performer_5.photo.attach(io: file, filename: 'chello.jpg', content_type: 'image/jpg')
+    performer_5.save!
+
 performer_6 = PerformerProfile.create!(
   user: User.create!(email: "performer6@email.com", password: "123456"),
   stage_name: "Stitch",
   bio: "A one man band playing the sax, guitar, piano, vocals and bass drums.  Originally from New Orleans.  I move to New York and then Meixco City. I love all kinds of music, I'm trained as a jazz player but also play blues, folk, gospel and country.  I love music and don't have a choice but to play, it's a calling that I have followed since a child."
 )
+file = URI.open("https://res.cloudinary.com/yaelmontufar/image/upload/v1583980523/User%20profiles/89144402_3211596228869719_8311786552791400448_n_ildipk.jpg")
+    performer_6.photo.attach(io: file, filename: 'chello.jpg', content_type: 'image/jpg')
+    performer_6.save!
+
 performer_7 = PerformerProfile.create!(
   user: User.create!(email: "performer7@email.com", password: "123456"),
   stage_name: "Copywrite",
   bio: "A busker of multiple countries and 10 years experience, Jack sillfylly mixes guitar, vocals, beatboxing, looping, sampling and effects to create a powerful sound evoking an eclectic blend of folk, hip hop, rock, pop and base music.  Captivating performance with a unique atmosphere and rich, driving layers, all performed live. Expect a hell of a show."
 )
+file = URI.open("https://res.cloudinary.com/yaelmontufar/image/upload/v1584030583/User%20profiles/pexels-photo-220453_z5pcrq.jpg")
+    performer_7.photo.attach(io: file, filename: 'chello.jpg', content_type: 'image/jpg')
+    performer_7.save!
+
+
 performer_8 = PerformerProfile.create!(
   user: User.create!(email: "performer8@email.com", password: "123456"),
   stage_name: "Stooge Elk",
   bio: "Jayden has been all over Mexico performing at festivals to exhibit his art.  Mexican-born Jayden is an amazing, super-talented street artist. Using tools like a power drill, chisel, and different types of paint, Jayden literally scratches off the surface of buildings to create his masterpieces."
 )
+file = URI.open("https://res.cloudinary.com/yaelmontufar/image/upload/v1584030580/User%20profiles/20181105_193836_idifid.jpg")
+    performer_8.photo.attach(io: file, filename: 'chello.jpg', content_type: 'image/jpg')
+    performer_8.save!
 
 # ========== upcoming events ==============
 
